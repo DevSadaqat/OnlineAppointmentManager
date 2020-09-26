b2212309d49dac7b976c5df6e9f34e7d862e315c
Sadaqat Ali <entrepreneurali@outlook.com>
2020-09-27 00:46:56 +1000

    First commit Added List component to display appointments Added Add compnnent form to make an appointment

---
+++ src/components/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+
+import {without} from 'lodash';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: [],
+      formDisplay: false,
+       lastIndex: 0
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+  }
+
+  //Event handler for adding component 
+  toggleForm() {
+   this.setState({
+     formDisplay: !this.state.formDisplay
+   }); 
+  }
+
+  //lodash method that return record array after deleting particular record 
+  deleteAppointment(apt) {
+let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    })
+
+  }
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.setState.lastIndex;
+          this.setState({lastIndex: this.setState.lastIndex + 1})
+          return item;
+          });
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments formDisplay = {this.state.formDisplay}
+                toggleForm={this.state.toggleForm} />
+                <SearchAppointments />
+                <ListAppointments appointments = {this.state.myAppointments}
+                deleteAppointment = {this.deleteAppointment} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchAppointments.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class SearchAppointments extends Component{
+    render() {
+        return <div>SearchComponent</div>;
+    }
+}
+
+export default SearchAppointments;
